62285f0be79be6bc0a290fc641604bb3f7988f33
Kaydarkey <darkeykafui@gmail.com>
2024-06-22 15:44:49 +0000

    Initial commit - setup project structure and basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # paul-leonard-video-platform
 A bespoke video hosting platform for Paul Leonard
+# Paul Leonard's Video Platform
+
+## Description
+A bespoke video hosting platform for Paul Leonard. This platform allows users to sign up, log in, watch videos, and for the admin to upload videos with titles and descriptions.
+
+## Features
+- User Signup
+- User Login
+- Video Navigation (Next/Previous)
+- Admin Video Upload
+- Password Reset
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/your-username/paul-leonard-video-platform.git
+   cd paul-leonard-video-platform
---
+++ contollers/videoController.js
@@ -0,0 +1,22 @@
+const Video = require('../models/Video');
+
+exports.getVideo = async (req, res) => {
+  const videoId = req.query.videoId || (await Video.findOne())._id;
+  const video = await Video.findById(videoId);
+  const videos = await Video.find();
+  const videoIndex = videos.findIndex(v => v._id.toString() === videoId);
+  res.render('video', {
+    video,
+    hasPrev: videoIndex > 0,
+    hasNext: videoIndex < videos.length - 1,
+    prevVideoId: videoIndex > 0 ? videos[videoIndex - 1]._id : null,
+    nextVideoId: videoIndex < videos.length - 1 ? videos[videoIndex + 1]._id : null,
+  });
+};
+
+exports.uploadVideo = async (req, res) => {
+  const { title, description, url } = req.body;
+  const video = new Video({ title, description, url });
+  await video.save();
+  res.redirect('/admin-upload');
+};
