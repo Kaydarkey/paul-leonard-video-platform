f4617cd1123171d6f0404e5243ef5e330123e10e
Kaydarkey <darkeykafui@gmail.com>
2024-06-23 23:57:09 +0000

    changes made to app.js and user

--- models/Video.js
+++ models/Video.js
@@ -1,9 +1,31 @@
+/*const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  // Add other video details as needed
+});
+
+const Video = mongoose.model('Video', videoSchema);
+
+module.exports = Video;
+*/
 const mongoose = require('mongoose');
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  url: String,
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Video', videoSchema);
+
