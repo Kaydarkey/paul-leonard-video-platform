dbeaaf30cb14b2d568f35acac9a76301bf8f3266
Kaydarkey <darkeykafui@gmail.com>
2024-06-28 21:27:49 +0000

    modified app.js

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/video', (req, res) => {
   if (!req.session.user) {
     return res.redirect('/login');
   }
