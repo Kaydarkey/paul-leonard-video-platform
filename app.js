8e688ee130dfd24542466383b271d55e2830fd1b
Kaydarkey <darkeykafui@gmail.com>
2024-07-16 12:36:32 +0000

    mofidied : app.js

--- app.js
+++ app.js
@@ -268,7 +268,7 @@ app.get('/', (req, res) => {
 });
 
 // Server start
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
 
